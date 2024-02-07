857062479c46a5d6d9da3faeeee63b118b48a6ae
Andrew Sikora <113684525+ASikora-HW@users.noreply.github.com>
2024-02-07 11:53:17 +0000

    Create assignment_1.ino
    - Initial upload of starter sketch

---
+++ assignment_1/assignment_1.ino
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+#define SIGNAL_A_PIN 21
+#define SIGNAL_B_PIN 23
+#define BUTTON_1_PIN 24
+#define BUTTON_2_PIN 25
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
