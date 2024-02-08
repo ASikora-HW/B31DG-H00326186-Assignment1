e0341734844f6b80652fbb22d9539210d2ac9722
Andrew Sikora <113684525+ASikora-HW@users.noreply.github.com>
2024-02-08 17:41:19 +0000

    Setup of pins and initialising variables
    - Set up the pins and pin modes for the pins that the signals will go through as well as the pins for the buttons.
    
    - Initialised the variables that will be used to calculate the pulse.
    
    - Just for checking, initialised the serial output to check that the variables had been stored correctly in the memory.

--- assignment_1/assignment_1.ino
+++ assignment_1/assignment_1.ino
@@ -1,16 +1,31 @@
 #include <Arduino.h>
 
-#define SIGNAL_A_PIN 21
-#define SIGNAL_B_PIN 23
-#define BUTTON_1_PIN 24
-#define BUTTON_2_PIN 25
+#define SIGNAL_A_PIN 23
+#define SIGNAL_B_PIN 15
+#define BUTTON_1_PIN 34
+#define BUTTON_2_PIN 12
+
+uint16_t a = 8 * 100; //S = 8
+uint16_t b = 9 * 100; //I = 9
+uint16_t c = 11 + 4; //K = 11
+uint16_t d = 12 * 500; //O = 12
+uint16_t mode = (9 % 4) + 1; //R = 9
+bool printedOutput = false;
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(SIGNAL_A_PIN, OUTPUT);
+  pinMode(SIGNAL_B_PIN, OUTPUT);
+  pinMode(BUTTON_1_PIN, INPUT);
+  pinMode(BUTTON_2_PIN, INPUT);
 
+  Serial.begin(9600);
+  while (!Serial); //initialise serial output
+  Serial.println("\n\nBeginning...");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if (!printedOutput) {
+    Serial.printf("A = %d\nB = %d\nC = %d\nD = %d\nMode = %d\n", a, b, c, d, mode);
+      printedOutput = true;
+  }
 }
