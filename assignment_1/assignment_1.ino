0120aa4def1117be7334a98a7d14c2cf2b04612e
Andrew Sikora <113684525+ASikora-HW@users.noreply.github.com>
2024-03-06 16:31:07 +0000

    Implementation of default signal
    - Debug mode conditional code-compile implemented
    
    - Pins reassigned to enable pull-down resistor of buttons
    
    - Initial implementation of default signal created
    
    - Simple button debounce added

--- assignment_1/assignment_1.ino
+++ assignment_1/assignment_1.ino
@@ -1,31 +1,87 @@
 #include <Arduino.h>
 
 #define SIGNAL_A_PIN 23
-#define SIGNAL_B_PIN 15
-#define BUTTON_1_PIN 34
-#define BUTTON_2_PIN 12
+#define SIGNAL_B_PIN 16
+#define BUTTON_1_PIN 14
+#define BUTTON_2_PIN 15
 
-uint16_t a = 8 * 100; //S = 8
-uint16_t b = 9 * 100; //I = 9
-uint16_t c = 11 + 4; //K = 11
-uint16_t d = 12 * 500; //O = 12
-uint16_t mode = (9 % 4) + 1; //R = 9
-bool printedOutput = false;
+// Debug mode select
+//#define DEBUG_MODE
+
+#ifdef DEBUG_MODE
+#define TIME_SLOW_FACTOR 1000
+#else
+#define TIME_SLOW_FACTOR 1
+#endif
+
+#define PULSE_DURATION ((8 * 100) * TIME_SLOW_FACTOR)          // microseconds
+#define BETWEEN_PULSE_OFF_TIME ((9 * 100) * TIME_SLOW_FACTOR)  // microseconds
+#define PULSE_COUNT (11 + 4)
+#define IDLE_TIME ((12 * 500) * TIME_SLOW_FACTOR)  // microseconds
+#define SYNC_ON_TIME (50 * TIME_SLOW_FACTOR)       // microseconds
+#define MODE 2
+
+// Signal variables
+bool outputEnabled = false;
+bool alternateMode = false;
+
+// Button state tracking
+bool lastEnableState = false;
+bool lastSelectState = false;
 
 void setup() {
   pinMode(SIGNAL_A_PIN, OUTPUT);
   pinMode(SIGNAL_B_PIN, OUTPUT);
-  pinMode(BUTTON_1_PIN, INPUT);
-  pinMode(BUTTON_2_PIN, INPUT);
-
+  pinMode(BUTTON_1_PIN, INPUT_PULLDOWN);
+  pinMode(BUTTON_2_PIN, INPUT_PULLDOWN);
   Serial.begin(9600);
-  while (!Serial); //initialise serial output
-  Serial.println("\n\nBeginning...");
 }
 
 void loop() {
-  if (!printedOutput) {
-    Serial.printf("A = %d\nB = %d\nC = %d\nD = %d\nMode = %d\n", a, b, c, d, mode);
-      printedOutput = true;
+  buttonDebounce();
+  if (outputEnabled) {
+    outputEnable();
   }
+}
+
+void buttonDebounce() {
+  bool currentEnableState = digitalRead(BUTTON_1_PIN);
+  bool currentSelectState = digitalRead(BUTTON_2_PIN);
+
+  // Check for state change with simple debounce logic
+  if (currentEnableState != lastEnableState && currentEnableState == HIGH) {
+    outputEnabled = !outputEnabled;  // Toggle output enable state
+    Serial.println(outputEnabled ? "Output Enabled\n" : "Output Disabled\n");
+  }
+
+  if (currentSelectState != lastSelectState && currentSelectState == HIGH) {
+    alternateMode = !alternateMode;  // Toggle output mode/state
+    Serial.println(alternateMode ? "Mode 2 Selected\n" : "Default Mode Selected\n");
+  }
+
+  // Update last states
+  lastEnableState = currentEnableState;
+  lastSelectState = currentSelectState;
+}
+
+void outputEnable() {
+  // Start SYNC signal
+  digitalWrite(SIGNAL_B_PIN, HIGH);
+  delayMicroseconds(SYNC_ON_TIME);
+  digitalWrite(SIGNAL_B_PIN, LOW);
+
+  //Default signal generation
+  for (int i = 0; i < PULSE_COUNT; i++) {
+    digitalWrite(SIGNAL_A_PIN, HIGH);
+    delayMicroseconds(PULSE_DURATION + (i * (50 * TIME_SLOW_FACTOR)));  //increase pulse duration for each pulse after 1st
+    digitalWrite(SIGNAL_A_PIN, LOW);
+
+    // Wait between pulses if not the last pulse
+    if (i < PULSE_COUNT - 1) {
+      delayMicroseconds(BETWEEN_PULSE_OFF_TIME);
+    }
+  }
+
+  // Idle time before next SYNC pulse
+  delayMicroseconds(IDLE_TIME);
 }
